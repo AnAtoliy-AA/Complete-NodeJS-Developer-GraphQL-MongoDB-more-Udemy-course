8bc660811a9d0a63b9bcbae4b58bc4f6d2c2d9a0
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-01-05 13:26:09 +0400

    feat: filter by max size and show planet names

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const CONFIRMED_FLAG = "CONFIRMED";
 const MAX_AMOUNT_OF_LIGHT = 1.11; //with higher value water on surface will disappear so fast
 const MIN_AMOUNT_OF_LIGHT = 0.36; //with lower value temperature gets far to low
 
-const MAX_PLANET_RADIUS = 1.6 // times bigger than Earth
+const MAX_PLANET_RADIUS = 1.6; // times bigger than Earth
 
 function isHabitablePlanet(planet) {
   return (
     planet["koi_disposition"] === CONFIRMED_FLAG &&
     planet["koi_insol"] > MIN_AMOUNT_OF_LIGHT &&
     planet["koi_insol"] < MAX_AMOUNT_OF_LIGHT &&
-    planet['koi_prad'] < MAX_PLANET_RADIUS
+    planet["koi_prad"] < MAX_PLANET_RADIUS
   );
 }
 
@@ -35,6 +35,7 @@ fs.createReadStream("kepler_data.csv")
     console.error(err);
   })
   .on("end", () => {
+    console.log(habitablePlanets.map((planet) => planet["kepler_name"]));
     console.log(`${habitablePlanets.length} habitable planets found!`);
     console.log("done");
   });
