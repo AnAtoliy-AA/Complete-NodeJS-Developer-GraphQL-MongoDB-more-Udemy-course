3580b94ab911e9e676340f6c5283f3d050700abf
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-01-05 13:24:18 +0400

    feat: add csv parser

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const fs = require("fs");
 const results = [];
 
 fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
   .on("data", (data) => {
     results.push(data);
   })
-  .on('error', (err) => {
+  .on("error", (err) => {
     console.error(err);
   })
   .on("end", () => {
     console.log(results);
     console.log("done");
   });
-
-// parse()
