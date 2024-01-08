f238f0bb3bdbde322a663f9221e81525a20dff73
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-01-08 16:43:16 +0400

    feat: basic routing

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/plain",
-  });
+const server = http.createServer();
 
-  res.end(JSON.stringify({ id: 1, text: "hi there!" }));
+server.on("request", (req, res) => {
+  if (req.url === "/friends") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+
+    res.end(JSON.stringify({ id: 1, text: "hi there!" }));
+  } else if (req.url === "/messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li> hi text</li>");
+    res.write("<li> some text</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
