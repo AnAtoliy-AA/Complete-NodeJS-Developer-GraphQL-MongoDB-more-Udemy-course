6b489c3382a800fa874214eee6e809b6ae6bb514
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-01-23 16:46:39 +0400

    feat: add frieands with friend id routes

--- server.js
+++ server.js
@@ -4,8 +4,22 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send({ id: 1, name: "some name" });
+const friends = [{ id: 0, name: "some name" }, { id: 1, name: "another name" }]
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId)
+
+  const friend = friends.find((friend) => friend.id === friendId)
+
+  if (!friend) {
+    res.status(404).json({ error: "No friends found" });
+  }
+
+  res.status(200).json(friend);
 });
 
 app.get("/messages", (req, res) => {
