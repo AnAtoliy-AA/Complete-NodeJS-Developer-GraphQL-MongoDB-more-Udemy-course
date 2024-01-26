4f19cd7badcb469d8e8ef32ec390a8eab1a504a8
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-01-26 15:49:08 +0400

    feat: post friend

--- server.js
+++ server.js
@@ -4,27 +4,49 @@ const app = express();
 
 const PORT = 3000;
 
-const friends = [{ id: 0, name: "some name" }, { id: 1, name: "another name" }]
+const friends = [
+  { id: 0, name: "some name" },
+  { id: 1, name: "another name" },
+];
 
 app.use((req, res, next) => {
   const startTime = Date.now();
- 
+
   next();
 
   const endTime = Date.now();
-  const delta = endTime - startTime
+  const delta = endTime - startTime;
 
-  console.log(`${req.method} ${req.url} executed ${delta} ms`)
-})
+  console.log(`${req.method} ${req.url} executed ${delta} ms`);
+});
+
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: "Missing friend name" });
+  }
+
+  const newFriend = {
+    name,
+    id: friends.length,
+  };
+
+  friends.push(newFriend);
+
+  res.json(newFriend);
+});
 
 app.get("/friends", (req, res) => {
   res.json(friends);
 });
 
 app.get("/friends/:friendId", (req, res) => {
-  const friendId = Number(req.params.friendId)
+  const friendId = Number(req.params.friendId);
 
-  const friend = friends.find((friend) => friend.id === friendId)
+  const friend = friends.find((friend) => friend.id === friendId);
 
   if (!friend) {
     res.status(404).json({ error: "No friends found" });
