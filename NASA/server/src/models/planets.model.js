80b4ed6754cdc3fad8d20bee9249bd79aaa4c340
Anatoliy <a.aliaksandrau@elinext.com>
2024-04-15 11:09:17 +0400

    feat: add kepler data for planets

--- NASA/server/src/models/planets.model.js
+++ NASA/server/src/models/planets.model.js
@@ -1,3 +1,51 @@
-const planets = [];
+const fs = require("fs");
+const path = require('path');
+const { parse } = require("csv-parse");
 
-module.exports = planets;
+const habitablePlanets = [];
+
+const CONFIRMED_FLAG = "CONFIRMED";
+
+const MAX_AMOUNT_OF_LIGHT = 1.11; //with higher value water on surface will disappear so fast
+const MIN_AMOUNT_OF_LIGHT = 0.36; //with lower value temperature gets far to low
+
+const MAX_PLANET_RADIUS = 1.6; // times bigger than Earth
+
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === CONFIRMED_FLAG &&
+    planet["koi_insol"] > MIN_AMOUNT_OF_LIGHT &&
+    planet["koi_insol"] < MAX_AMOUNT_OF_LIGHT &&
+    planet["koi_prad"] < MAX_PLANET_RADIUS
+  );
+}
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        console.error(err);
+        reject(err);
+      })
+      .on("end", () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve()
+      });
+  })
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets
+};
