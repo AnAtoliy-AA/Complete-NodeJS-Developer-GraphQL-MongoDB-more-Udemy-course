1d22ad77d7d33754a37c40a8719895ae5feeb129
Anatoliy <a.aliaksandrau@elinext.com>
2024-04-17 08:17:20 +0400

    feat: post launches endpoint

--- NASA/server/src/models/launches.model.js
+++ NASA/server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
@@ -17,6 +19,17 @@ function getAllLaunches() {
     return Array.from(launches.values())
 }
 
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(latestFlightNumber, Object.assign(launch, {
+        flightNumber: latestFlightNumber,
+        customer: ['Zero To Mastery, NASA'],
+        upcoming: true,
+        success: true,
+    }));
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
