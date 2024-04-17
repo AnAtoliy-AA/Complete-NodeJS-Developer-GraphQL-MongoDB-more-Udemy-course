8c3501f0733f711ce8baed6b3eded11388592cbc
Anatoliy <a.aliaksandrau@elinext.com>
2024-04-17 08:33:55 +0400

    feat: post launches validation

--- NASA/server/src/routes/launches/launches.controller.js
+++ NASA/server/src/routes/launches/launches.controller.js
@@ -7,7 +7,23 @@ function httpGetAllLaunches(_, res) {
 function httpAddNewLaunch(req, res) {
     const launch = req.body
 
+    if (!launch.mission
+        || !launch.rocket
+        || !launch.launchDate
+        || !launch.destination) {
+        return res.status(400).json({
+            error: 'Missing required launch property',
+        })
+    }
+
     launch.launchDate = new Date(launch.launchDate);
+
+    if (isNaN(launch.launchDate)) {
+    // if (launch.launchDate.toString() === 'Invalid Date') {
+        return res.status(400).json({
+            error: 'Invalid Launch Date',
+        })
+    }
 
     addNewLaunch(launch)
 
