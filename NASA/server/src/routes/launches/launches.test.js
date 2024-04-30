0893c08a2022ca025587335f0dfda1a0456534a9
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-04-30 18:03:37 +0400

    feat: post unit tests

--- NASA/server/src/routes/launches/launches.test.js
+++ NASA/server/src/routes/launches/launches.test.js
@@ -12,8 +12,32 @@ describe("Test GET /launches", () => {
   });
 });
 
-describe("Test POST /launch", () => {
-  test("It should respond with 200 success", () => {});
+describe("Test POST /launches", () => {
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+    launchDate: "January 4, 2028",
+  };
+
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-186 f",
+  };
+
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+    expect(responseDate).toBe(requestDate);
+  });
 
   test("It should catch missing required properties", () => {});
 
