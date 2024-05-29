02647533e66c1dd930540dffb7103c80fdc0bd5b
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-05-29 17:14:57 +0400

    feat: connect to mongo db with mongoose

--- NASA/server/src/server.js
+++ NASA/server/src/server.js
@@ -1,18 +1,30 @@
 const http = require('http');
-const app = require("./app")
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = require("./app");
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@nasacluster.wsrnuaq.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster`;
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => console.log('Mongoose connection'));
+
+mongoose.connection.on('error', (err) => console.error(err));
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
+
     await loadPlanetsData();
 
     server.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}...`)
-    })
+        console.log(`Listening on port ${PORT}...`);
+    });
 };
 
 startServer();
