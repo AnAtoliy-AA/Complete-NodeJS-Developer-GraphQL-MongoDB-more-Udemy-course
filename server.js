afdc7df94fa400e5c1af4ba245112979dfb2159c
aaliaksandrau <a.aliaksandrau@elinext.com>
2024-11-01 14:59:49 +0400

    feat: add self signed certificates

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const https = require("https");
 const path = require("path");
 const express = require("express");
 
@@ -5,10 +7,19 @@ const PORT = 3000;
 
 const app = express();
 
+app.get("/secret", (req, res) => {
+  return res.send("Your secret value is 42~!");
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+https
+  .createServer({
+    key: fs.readFileSync("key.pem"),
+    cert: fs.readFileSync("cert.pem"),
+  }, app)
+  .listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+  });
